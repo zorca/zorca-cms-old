ae8277019c68c8b1686adabcd8fb572906f9db27
Zorca <vs@zorca.org>
2015-09-09 13:29:04 +0300

    Третий урок: добавляем страницы

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
+require_once __DIR__ . '/init.php';
 
-require_once __DIR__ . '/vendor/autoload.php';
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$request = Request::createFromGlobals();
 $input = $request->get('name', 'Default');
 
-$response = new Response;
 $response->setContent(sprintf('Hello, %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
 $response->prepare($request);
 $response->send();
