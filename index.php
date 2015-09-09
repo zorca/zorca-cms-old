55488a16fd837a26a0f0bf8a727dc31c470d1791
Zorca <vs@zorca.org>
2015-09-09 12:32:46 +0300

    Урок второй: используем компонент HttpFoundation

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-$input = $_GET['name'];
+require_once __DIR__ . '/vendor/autoload.php';
 
-printf('Hello, %s', $input);
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$input = $request->get('name', 'Default');
+
+$response = new Response;
+$response->setContent(sprintf('Hello, %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+$response->prepare($request);
+$response->send();
