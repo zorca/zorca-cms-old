ae2f9e94d141eab29139053a66596b07a7fd6661
Zorca <vs@zorca.org>
2015-09-09 18:24:54 +0300

    Урок четыре: маршрутизация через компонент Routing

--- hello.php
+++ hello.php
@@ -1,2 +1,1 @@
-<?php $name = $request->get('name', 'Иван Иввнов') ?>
 Здравствуй, <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
--- index.php
+++ index.php
@@ -1,20 +1,31 @@
 <?php
-require_once __DIR__ . '/init.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-$routeMap = array(
-    '/hello' => 'hello',
-    '/bye'   => 'bye',
-);
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
 
-$path = $request->getPathInfo();
-if (isset($routeMap[$path])) {
+$request = Request::createFromGlobals();
+
+$response = new Response;
+
+$routes = include 'routes.php';
+
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    include sprintf('%s.php', $routeMap[$path]);
-    $response->setContent(ob_get_clean());
-} else {
-    $response->setStatusCode(404);
-    $response->setContent('Страница не найдена');
+    include sprintf('%s.php', $_route);
+    $response = new Response(ob_get_clean());
+} catch (Routing\Exception\ResourceNotFoundException $e) {
+    $response = new Response('Страница не найдена', 404);
+} catch (Exception $e) {
+    $response = new Response('Обнаружена ошибка', 500);
 }
+
 $response->prepare($request);
 $response->send();
