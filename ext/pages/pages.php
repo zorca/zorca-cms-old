f010e9283ba8859201db1154ba8d916883f7c050
Zorca <vs@zorca.org>
2015-09-14 11:32:58 +0300

    Добавлены скелеты темы

--- ext/pages/pages.php
+++ ext/pages/pages.php
@@ -9,9 +9,12 @@ class PagesExt {
     public function run($extAction) {
         $parsedown = new ParsedownExtra();
         $templates = new Twig_Loader_Filesystem(BASE . 'pub/themes/default/templates');
+        $skeletons = new Twig_Loader_Filesystem(BASE . 'pub/skeletons/default/templates');
         $twig = new Twig_Environment($templates);
+        $twigSkeleton = new Twig_Environment($skeletons);
         $pageContentFilePath = BASE .  'data/pages' . DS . $extAction . '.md';
         $pageContent = $parsedown->text(file_get_contents($pageContentFilePath));
-        return $twig->render('pages.twig', ['pageContent' => $pageContent]);
+        $skeleton = $twigSkeleton->loadTemplate('default.twig');
+        return $twig->render('pages.twig', ['pageContent' => $pageContent, 'skeleton' => $skeleton]);
     }
 }
