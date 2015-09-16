6522f9d59d800e0d9f38f0ca7c0b9128399c19f5
Zorca <vs@zorca.org>
2015-09-16 17:43:04 +0300

    Урок 8: меняем структуру папок

--- zorca/Config.php
+++ zorca/Config.php
@@ -3,18 +3,9 @@ namespace Zorca;
 
 class Config {
     static function load($configParam) {
-        // Если не указано, какой конфиг загрузить, загружаем основной
-        if (!is_null($configParam)) {
-            $configFilePath = BASE . $configParam . DS . $configParam . '.json';
-        } else {
-            $configFilePath = BASE . 'data/config.json';
-        }
+        $configFilePath = BASE . $configParam . DS . $configParam . '.json';
         // Если файл конфига не существует, то отдаем пустой массив
-        if (file_exists($configFilePath)) {
-            $config = json_decode(file_get_contents($configFilePath), true);
-        } else {
-            $config = [];
-        }
+        if (file_exists($configFilePath)) $config = json_decode(file_get_contents($configFilePath), true); else $config = [];
         return $config;
     }
 }
