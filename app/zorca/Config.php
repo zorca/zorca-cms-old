fce169e87aa8f1e53f27b6a11f87ae3197db5076
Zorca <vs@zorca.org>
2015-09-18 14:09:46 +0300

    Фикс конфигов

--- app/zorca/Config.php
+++ app/zorca/Config.php
@@ -3,7 +3,7 @@ namespace Zorca;
 
 class Config {
     static function load($configParam) {
-        if ($configParam === 'app') $configFilePath = APP . 'config.json'; else $configFilePath = APP . $configParam . DS . $configParam . '.json';
+        if ($configParam === 'app') $configFilePath = DATA . 'config.json'; else $configFilePath = DATA . $configParam . '.json';
         // Если файл конфига не существует, то отдаем пустой массив
         if (file_exists($configFilePath)) $config = json_decode(file_get_contents($configFilePath), true); else $config = [];
         return $config;
