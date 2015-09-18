b79d8b7f334426d0a01edbed658956bea4a40393
Zorca <vs@zorca.org>
2015-09-18 13:29:06 +0300

    DebugBar fix

--- app/zorca/Theme.php
+++ app/zorca/Theme.php
@@ -17,7 +17,7 @@ class Theme {
             $debugbarFoot = '';
         }
         $templates = new Twig_Loader_Filesystem(APP . 'design/themes' . DS . $mainConfig['theme'] . DS . 'templates/ext');
-        $skeletons = new Twig_Loader_Filesystem(APP . 'design/skeletons/default');
+        $skeletons = new Twig_Loader_Filesystem(APP . 'design/skeletons/' . $mainConfig['skeleton']);
         $twigTemplate = new Twig_Environment($templates);
         $twigSkeleton = new Twig_Environment($skeletons);
         $skeleton = $twigSkeleton->loadTemplate($mainConfig['skeleton'] . '.twig');
