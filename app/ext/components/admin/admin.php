cf1408cd3decab9021bb29ed03e7e0913e5c4621
Zorca <vs@zorca.org>
2015-09-18 13:43:04 +0300

    Добавил тестовый View для разработки

--- app/ext/components/admin/admin.php
+++ app/ext/components/admin/admin.php
@@ -4,6 +4,11 @@ namespace Zorca\Ext;
 use Symfony\Component\HttpFoundation\Response;
 class AdminExt {
     public function run($extAction) {
+        $responseStatus = '200';
+        if ($extAction === 'login') $this->login($extAction);
         return new Response('Расширение Admin работает');
+    }
+    private function login($extAction) {
+        require_once APP . 'ext/components/admin/views/login.php';
     }
 }
---
+++ app/ext/components/admin/views/login.php
@@ -0,0 +1,12 @@
+<form  method="POST" action="">
+    <input type="hidden" name="token" value="Token">
+    <div class="row">
+        <div class="box-3 col"></div>
+        <div class="box-3 col">
+            <label>Login</label>
+            <input class="pull-left" type="password" name="password" id="password" placeholder="Password">
+            <input class="pull-left" type="submit" class="btn" value="Login">
+        </div>
+        <div class="box-3 col"></div>
+    </div>
+</form>
