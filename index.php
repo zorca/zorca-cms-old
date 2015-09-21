3dd1d8ae0a7bdffd0cb43941cfac4104aa92e616
Zorca <vs@zorca.org>
2015-09-21 19:33:57 +0300

    Изменения в блокировке доступа к файлам

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
-/**
- * @todo Сделать режимы запуска: development, production
- */
+
 // Проверяем версию PHP
 if (version_compare($ver = PHP_VERSION, $req = '5.4.0', '<')) {
-    throw new \RuntimeException(sprintf('You are running PHP %s, but Zorca needs at least <strong>PHP %s</strong> to run.', $ver, $req));
+    throw new \RuntimeException(sprintf("You are running PHP %s, but Zorca needs at least <strong>PHP %s</strong> to run.", $ver, $req));
 }
 
 // Устанавливаем временную зону по-умолчанию, на случай, если она не установлена в php.ini
 date_default_timezone_set(@date_default_timezone_get());
 
-// Проверяем установлено ли расширение mbstring
+// Проверяем установлено ли расширение PHP mbstring
 if (!extension_loaded('mbstring')) {
-    throw new \RuntimeException("'mbstring' extension is not loaded.  This is required for Zorca to run correctly");
+    throw new \RuntimeException("Extension 'mbstring' extension is not loaded.  This is required for Zorca to run correctly");
 }
 mb_internal_encoding('UTF-8');
 
@@ -26,7 +24,7 @@ define('PUB', BASE . 'pub' . DS);
 // Проверяем, установлены ли необходимые библиотеки
 $autoload = __DIR__ . '/vendor/autoload.php';
 if (!is_file($autoload)) {
-    throw new \RuntimeException('Please run: <i>composer install</i>');
+    throw new \RuntimeException("Please run: <i>composer install</i>");
 }
 
 // Подгружаем библиотеки
