ff7ad9c7addf07cc5cf9cb75b68a9682f7a71e27
Alexus <alexey.shapilov@gmail.com>
2015-11-16 10:49:18 +0300

    Оптимизация кода обработки стилей

--- app/core/zorca/Scss.php
+++ app/core/zorca/Scss.php
@@ -7,6 +7,7 @@
     {
 
         private $importPath = [];
+        private $scss = '';
 
         public function compile($scss) {
             $compiler = new Compiler();
@@ -17,35 +18,40 @@
         }
 
         public function compileFile($in, $out) {
-            $scss = '@charset "utf-8";';
+            $this->scss = '@charset "utf-8";';
             foreach ($in as $inItem) {
-                if (file_exists($inItem)) {
-                    $scss = $scss . PHP_EOL . file_get_contents($inItem);
-                    $this->importPath($inItem);
-                }
+                $this->importPath($inItem);
             }
 
-            $css = $this->compile($scss);
+            $css = $this->compile($this->scss);
             $css = csscrush_string($css, ['formatter' => 'block']);
             if (!file_exists($out)) {
                 if (!file_exists(dirname($out))) {
                     mkdir(dirname($out), 0775, true);
                 }
-            }l
+            }
             return file_put_contents($out, $css);
         }
 
         private function importPath($scssFile) {
-            if (file_exists($scssFile)) $scss = file_get_contents($scssFile);
-            preg_match_all('%(//)?.*@import\s*"(.*)"%i', $scss, $result, PREG_PATTERN_ORDER);
-            for ($i = 0; $i < count($result[0]); $i++) {
-                if (empty($result[1][$i])) {
-                    $path = dirname($scssFile) . DS . $result[2][$i];
-                    if (!in_array(dirname($scssFile), $this->importPath)) {
-                        $this->importPath[] = dirname($scssFile);
-                    }
-                    if (file_exists($pathRelateImport = $path) || file_exists($pathRelateImport = $path . '.scss') || file_exists($pathRelateImport = dirname($path) . '/_' . basename($path) . '.scss')) {
-                        $this->importPath($pathRelateImport);
+            $scss = '';
+            $path = pathinfo($scssFile);
+            if (file_exists($pathRelateImport = $scssFile) ||
+                file_exists($pathRelateImport = (isset($path['extension']) ? $scssFile . '.scss' : '')) ||
+                file_exists($pathRelateImport = dirname($scssFile) . '/_' . basename($scssFile) . (isset($path['extension']) ? $scssFile . '.scss' : ''))
+            ) {
+                $scss = file_get_contents($scssFile);
+            }
+            if (!empty($scss)) {
+                $this->scss .= PHP_EOL . $scss;
+                preg_match_all('%(//)?.*@import\s*"(.*)"%i', $scss, $result, PREG_PATTERN_ORDER);
+                for ($i = 0; $i < count($result[0]); $i++) {
+                    if (empty($result[1][$i])) {
+                        $pathImport = dirname($scssFile) . DS . $result[2][$i];
+                        if (!in_array(dirname($scssFile), $this->importPath)) {
+                            $this->importPath[] = dirname($scssFile);
+                        }
+                        $this->importPath($pathImport);
                     }
                 }
             }
